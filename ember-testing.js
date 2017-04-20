d412e606ef1c86aabacdca1a61c08ad9c8a03f3f
Brandon Drake <brandon.drake@teem.com>
2017-04-20 08:33:09 -0600

    Added ember-testing linting

---
+++ ember-testing.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: 'teem/ember',
+  rules: {
+  },
+  globals: {
+    'checkBox':                           false,
+    'cleanupWithTitle':                   false,
+    'clickPowerSelect':                   false,
+    'createUntil':                        false,
+    'createWithTitle':                    false,
+    'forEachMatching':                    false,
+    'navigateToResource':                 false,
+    'searchForCard':                      false,
+    'selectChoose':                       false,
+    'selectSearch':                       false,
+    'shouldBeCheckedAsync':               false,
+    'shouldHaveClassAsync':               false,
+    'shouldHaveElementWithAtLeastCount':  false,
+    'shouldHaveElementWithCount':         false,
+    'shouldHavePageElements':             false,
+    'shouldHaveText':                     false,
+    'shouldHaveVal':                      false,
+    'thenDo':                             false,
+  }
+};
